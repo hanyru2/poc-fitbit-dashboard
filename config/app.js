aa28a40791783885fcd828cdd3e3bdd1632d434c
Wichit Yaowapapong <>
2020-09-08 16:18:41 +0700

    Add heart rate and steps line chart.

--- config/app.js
+++ config/app.js
@@ -1,4 +1,4 @@
 exports.CLIENT_ID = process.env.CLIENT_ID || "22BRTN"
-exports.CLIENT_SECRET = process.env.CLIENT_SECRET || "fa44a535af78908cb028c1f7c458ed3e"
+exports.CLIENT_SECRET = process.env.CLIENT_SECRET || "6e8c60581e26b02d8b7b66e70cc5d7a6"
 exports.SESSION_SECRET = process.env.SESSION_SECRET || "fitbit"
 exports.CALLBACK_URL = process.env.CALLBACK_URL || "http://localhost:3000/callback"
